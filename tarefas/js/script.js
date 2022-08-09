bd08e512f4c7c99d785742351a906c6771492734
Diego Rapoport <diegomrapoport@gmail.com>
2022-08-08 21:54:25 -0300

    Deleta código desnecessário

--- tarefas/js/script.js
+++ tarefas/js/script.js
@@ -68,28 +68,3 @@ document.getElementById("adicionarBotao").addEventListener('click', function adi
     tarefa.value = ''
   }
 })
-
-var modals = document.querySelectorAll(".modal-open");
-
-modals.forEach((element) => {
-  //Add click listeners to open-modal buttons
-  element.addEventListener("click", () => {
-
-    //Get the unique popup modal for that button
-    var popupModal = document.getElementById(element.dataset.popupTrigger);
-
-    //Select the close button of current popup modal
-    var popupModalCloseButton = document
-      .getElementById(element.dataset.popupTrigger)
-      .getElementsByClassName("modal-close-button")[0];
-
-    // Show/hide modal on click by toggling the class
-    popupModal.classList.toggle("show-modal");
-
-    //Add event listener to close button on popup modal.
-    popupModalCloseButton.addEventListener("click", () => {
-      popupModal.classList.remove("show-modal");
-    });
-
-  });
-});
